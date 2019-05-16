f5b75fab7aac85bf2fc96979441913ae0ad84c92
Joshua Crum <joshua.crum@gmail.com>
2019-05-16 20:34:13 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -7,3 +7,18 @@ class Person
   attr_accessor :name
 
 end
+
+class Dog
+  def initialize(name)
+    @name = name
+  end
+
+  attr_accessor :name
+
+  def initialize(breed)
+    @breed = breed
+  end
+
+  attr_accessor :breed
+
+end
