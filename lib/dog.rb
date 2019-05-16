a80cb5be69f40835af3d74c1aff2e42cbc0d7072
Joshua Crum <joshua.crum@gmail.com>
2019-05-16 20:49:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,9 @@
 class Dog
 
-   def initialize(name)
+  def initialize(name)
     @name = name
   end
 
-   def name=(name)
-    @name = name
-  end
+  attr_accessor :name
 
-   def name
-    @name
-  end
-
-  def initialize(breed)
-    @breed = breed
-  end
-
-   def breed=(breed)
-    @breed = breed
-  end
-
-   def breed
-    @breed
-  end
-
- end
+end
