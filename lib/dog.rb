a7d28c49453fc8f19406d22729df5912de951578
Joshua Crum <joshua.crum@gmail.com>
2019-05-16 20:43:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,7 @@
 class Dog
 
-def initialize(name)
-    @name = name
-  end
-
-def initialize(breed)
-    @breed = breed
-  end
-
+def initialize(name, breed="Mutt")
+  name = @name
+  breed =@breed
+end
 end
